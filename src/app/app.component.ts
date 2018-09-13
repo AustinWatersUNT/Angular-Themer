f9ca98ac26385ff3fd215a5c482f496e58141b64
Austin Waters <austin.waters@sync1systems.com>
2018-09-13 14:44:25 -0500

    Created Angular Themer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-themer';
+export class AppComponent implements OnInit {
+  theme = 'sync1-theme';
+  previousTheme = 'sync1-theme';
+  parent: any;
+
+  themes: {theme: string, display: string}[] = [
+    {theme: 'sync1-theme', display: 'Sync1 Theme'},
+    {theme: 'sync1-mixin-theme', display: 'Sync1 Mixin Theme'},
+    {theme: 'custom-light-theme', display: 'Custom Light Theme'}
+  ];
+
+  prebuiltThemes: {theme: string, display: string}[] = [
+    {theme: 'deeppurple-amber', display: 'Deep Purple / Amber'},
+    {theme: 'indigo-pink', display: 'Indigo / Pink'},
+    {theme: 'pink-bluegrey', display: 'Pink / Blue Grey'},
+    {theme: 'purple-green', display: 'Purple / Green'},
+  ];
+
+  constructor(
+    private overlayContainer: OverlayContainer
+  ) {}
+
+  ngOnInit(): void {
+    this.parent = this.overlayContainer.getContainerElement().parentElement;
+  }
+
+  changeTheme = () => {
+    this.parent.classList.replace(this.previousTheme, this.theme);
+    this.previousTheme = this.theme;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { AngularMaterialModule } from '../shared/angular-material.module';
 
 import { AppComponent } from './app.component';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(overlayContainer: OverlayContainer) {
+    // Adds theme to the body
+    overlayContainer.getContainerElement().parentElement.classList.add('sync1-theme');
+  }
+}
